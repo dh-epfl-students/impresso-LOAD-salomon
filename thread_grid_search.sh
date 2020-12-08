8d91a4fcc25abbf3dea63b91a1d6b4f5696556b1
jdsalomon <julien.salomon@epfl.ch>
2020-12-08 15:09:11 +0100

    grid search added

---
+++ thread_grid_search.sh
@@ -0,0 +1,18 @@
+java -Xmx20g -jar artifacts/impresso_LOAD_salomon_jar/impresso-LOAD-salomon_t1.jar "GDL"
+mv log.txt log_t1.txt
+java -Xmx20g -jar artifacts/impresso_LOAD_salomon_jar/impresso-LOAD-salomon_t5.jar "GDL"
+mv log.txt log_t5.txt
+java -Xmx20g -jar artifacts/impresso_LOAD_salomon_jar/impresso-LOAD-salomon_t10.jar "GDL"
+mv log.txt log_t10.txt
+java -Xmx20g -jar artifacts/impresso_LOAD_salomon_jar/impresso-LOAD-salomon_t20.jar "GDL"
+mv log.txt log_t20.txt
+java -Xmx20g -jar artifacts/impresso_LOAD_salomon_jar/impresso-LOAD-salomon_t30.jar "GDL"
+mv log.txt log_t30.txt
+java -Xmx20g -jar artifacts/impresso_LOAD_salomon_jar/impresso-LOAD-salomon_t40.jar "GDL"
+mv log.txt log_t40.txt
+java -Xmx20g -jar artifacts/impresso_LOAD_salomon_jar/impresso-LOAD-salomon_t48.jar "GDL"
+mv log.txt log_t48.txt
+java -Xmx20g -jar artifacts/impresso_LOAD_salomon_jar/impresso-LOAD-salomon_t50.jar "GDL"
+mv log.txt log_t50.txt
+java -Xmx20g -jar artifacts/impresso_LOAD_salomon_jar/impresso-LOAD-salomon_t55.jar "GDL"
+mv log.txt log_t55.txt
